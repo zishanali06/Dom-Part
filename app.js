81d9c2bbe0e2b5ad803824987af7bc58283e4019
Zishan Ali <zishanali06@gmail.com>
2019-03-03 21:41:36 -0600

    done with Dom side of this

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(){
     let button = document.createElement('button');
-    let buttontxt = document.createTextNode('Click Me');
+    let buttontxt = document.createTextNode('Click Me for Alert');
 
     button.appendChild(buttontxt);
     document.body.appendChild(button);
@@ -54,4 +54,21 @@ newdiv.addEventListener('mouseover', function(){
 newdiv.addEventListener('mouseleave', function(){
     newdiv.style.backgroundColor = 'blue';
 })
+
+let friends = ['Ashley', 'Armeen', 'Fehzan', 'James', 'Tim', 'Chris', 'Andy', 'Nick', 'GC', 'Alex'];
+let button4 = document.querySelector('.btn2');
+let ul = document.querySelector('ul');
+
+button4.addEventListener('click', function(){
+    for(i = 0; i < friends.length; i++){
+        let newli = document.createElement('li');
+        let litext = document.createTextNode(friends[i]);
+
+        newli.appendChild(litext);
+        ul.appendChild(newli);
+
+    }
+});
+
+
 
